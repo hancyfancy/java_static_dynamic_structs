a87d3f8b07691aea3c04dc70705e2d03f8fe1463
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-07-04 11:18:43 +1000

    Add structure to store unique items

---
+++ DynamicUniqueItemStore.java
@@ -0,0 +1,26 @@
+public class DynamicUniqueItemStore<T> extends DynamicItemStore<T> {
+    public DynamicUniqueItemStore() {
+        super();
+    }
+    @Override
+    public void setItem(Item<?> newItem) {
+        if (getIndex(newItem) == -1) {
+            Item<?>[] newItems = null;
+            int currentIndex = getCurrentIndex();
+            int nextIndex = currentIndex + 1;
+            Item<?>[] items = getItems();
+            int itemsLength = items.length;
+            int nextItemsLength = itemsLength + 1;
+            newItems = new Item[nextItemsLength];
+            for (int i = 0; i < itemsLength; i++) {
+                Item<?> currentItem = items[i];
+                if (currentItem != null) {
+                    newItems[i] = currentItem;
+                }
+            }
+            newItems[currentIndex] = newItem;
+            super.setItems(newItems);
+            super.setCurrentIndex(nextIndex);
+        }
+    }
+}
