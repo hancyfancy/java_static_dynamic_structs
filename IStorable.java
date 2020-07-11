51502bc16cf454cd3090307d14d29c2d9863d355
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-07-11 13:28:33 +1000

    Expose capability to get item by index

--- IStorable.java
+++ IStorable.java
@@ -1,5 +1,6 @@
 interface IStorable<T> {
     public int getIndex(Item<?> item);
     public Item<T> getCurrentItem();
+    public Item<T> getItem(int index);
     public void setItem(Item<?> newItem);
 }
