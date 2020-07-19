d497097ae9f76f66df27e6f3f5b7dd665a088602
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-07-19 15:41:09 +1000

    Distribute exposed services

--- DynamicItemStore.java
+++ DynamicItemStore.java
@@ -1,9 +1,9 @@
-public class DynamicItemStore<T> extends AbstractItemStore<T> implements IStorable<T> {
+public class DynamicItemStore<T> extends AbstractItemStore<T> implements IInsertable<T>, IIterable<T>, IOneDimensionStorable<T> {
     public DynamicItemStore() {
         super();
         super.setItems(new Item[]{});
     }
-    public int getIndex(Item<?> item) {
+    private int getIndex(Item<?> item) {
         int index = -1;
         Item<?>[] items = getItems();
         for (int i = 0; i < items.length; i++) {
@@ -14,10 +14,13 @@ public class DynamicItemStore<T> extends AbstractItemStore<T> implements IStorab
         }
         return index;
     }
-    public Item<T> getCurrentItem() {
+    public int getIndex(Object object) {
+        return getIndex(new Item(object));
+    }
+    private Item<T> getCurrentItem() {
         return (Item<T>)getItems()[getCurrentIndex()];
     }
-    public void setItem(Item<?> newItem) {
+    protected void setItem(Item<?> newItem) {
         Item<?>[] newItems = null;
         int currentIndex = getCurrentIndex();
         int nextIndex = currentIndex + 1;
@@ -35,11 +38,48 @@ public class DynamicItemStore<T> extends AbstractItemStore<T> implements IStorab
         super.setItems(newItems);
         super.setCurrentIndex(nextIndex);
     }
-    public void replaceItem(int index, Item<?> newItem) {
+    public void add(Object toBeAdded) {
+        setItem(new Item(toBeAdded));
+    }
+    private void replaceItem(int index, Item<?> newItem) {
         Item<?>[] items = getItems();
         if (index < items.length) {
             items[index] = newItem;
             super.setItems(items);
         }
+    }
+    public void replace(int index, Object toBeReplaced) {
+        replaceItem(index, new Item(toBeReplaced)); 
+    }
+    private void insertItem(int index, Item<?> newItem) {
+        Item<?>[] newItems = null;
+        Item<?>[] items = getItems();
+        int itemsLength = items.length;
+        if (index > -1) {
+            if (index >= itemsLength) {
+                super.setCurrentIndex(itemsLength);
+                setItem(newItem);
+            } else {
+                newItems = new Item[itemsLength+1];
+                if (index == 0) {
+                    newItems[index] = newItem;
+                    for (int i = index+1; i < newItems.length; i++) {
+                        newItems[i] = items[i-1];
+                    }
+                } else { //index > 0 && index < itemsLength
+                    for (int j = 0; j < index; j++) {
+                        newItems[j] = items[j];
+                    }
+                    newItems[index] = newItem;
+                    for (int k = index; k < items.length; k++) {
+                        newItems[k+1] = items[k];
+                    }
+                }
+                super.setItems(newItems);
+            }
+        }
+    }
+    public void insert(int index, Object toBeInserted) {
+        insertItem(index, new Item(toBeInserted));
     }
 }
