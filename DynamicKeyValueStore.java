7243896f5127dd0d1f08a2eca58c92085e02c216
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-07-19 15:38:09 +1000

    Distribute exposed services

--- DynamicKeyValueStore.java
+++ DynamicKeyValueStore.java
@@ -1,29 +1,32 @@
-public class DynamicKeyValueStore<K,V> implements ISortable<K> {
+public class DynamicKeyValueStore<K,V> implements IIterable<K>, ISortable<K>, ITwoDimensionStorable<K> {
     private DynamicItemStore<?> keys;
     private DynamicItemStore<?> values;
     public DynamicKeyValueStore() {
         this.setKeys(new DynamicItemStore<K>());
         this.setValues(new DynamicItemStore<V>());
     }
-    public DynamicItemStore<K> getKeys() {
+    private DynamicItemStore<K> getKeys() {
         return (DynamicItemStore<K>)this.keys;
     }
     private void setKeys(DynamicItemStore<?> newKeys) {
         this.keys = newKeys;
     }
-    public DynamicItemStore<V> getValues() {
+    private DynamicItemStore<V> getValues() {
         return (DynamicItemStore<V>)this.values;
     }
     private void setValues(DynamicItemStore<?> newValues) {
         this.values = newValues;
     }
-    public int index(Item<?> key) {
+    private int getIndex(Item<?> key) {
         return getKeys().getIndex(key);
     }
-    public Item<V> get(int index) {
+    public int getIndex(Object object) {
+        return getIndex(new Item(object));
+    }
+    private Item<V> getValue(int index) {
         return getValues().getItem(index);
     }
-    public Item<V> get(Item<?> key) {
+    private Item<V> getValue(Item<?> key) {
         Item<?> value = null;
         DynamicItemStore<?> keys = getKeys();
         Item<?>[] keyItems = keys.getItems();
@@ -35,13 +38,33 @@ public class DynamicKeyValueStore<K,V> implements ISortable<K> {
         }
         return (Item<V>)value;
     }
-    public void set(Item<?> key, Item<?> value) {
+    public Object get(Object key, boolean byKey) {
+        Object value = null;
+        if (!byKey) {
+            if (key.getClass().getName().equals("java.lang.Integer")) {
+                int index = (int)key;
+                if ((index > -1) && (index < getLength())) {
+                    value = getValue(index).toObject();
+                }
+            }
+        } else {
+            value = getValue(new Item(key)).toObject();
+        }
+        return value;
+    }
+    public int getLength() {
+        return getKeys().getLength();
+    }
+    private void set(Item<?> key, Item<?> value) {
         DynamicItemStore<?> keys = getKeys();
         DynamicItemStore<?> values = getValues();
         keys.setItem(key);
         values.setItem(value);
         this.setKeys(keys);
         this.setValues(values);
+    }
+    public void add(Object key, Object value) {
+        set(new Item(key), new Item(value));
     }
     private void sort(boolean createSortOrder) {
         DynamicItemStore<?> originalKeys = new DynamicItemStore<K>();
