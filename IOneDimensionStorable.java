1968350f21e264de8b2cb14d6f344eeb3cd3d902
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-07-26 17:32:09 +1000

    Preserve type correctness when accessing

--- IOneDimensionStorable.java
+++ IOneDimensionStorable.java
@@ -1,5 +1,5 @@
 interface IOneDimensionStorable<T> {
-    public Object get(int index);
+    public T get(int index);
     public void add(Object toBeAdded);
     public void replace(int index, Object toBeReplaced);
 }
