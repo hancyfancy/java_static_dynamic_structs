383181c529b96119371ab17ce883b0d4264c1b75
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-08-02 16:38:47 +1000

    Add replaceAll method

--- IOneDimensionStorable.java
+++ IOneDimensionStorable.java
@@ -2,4 +2,5 @@ interface IOneDimensionStorable<T> {
     public T get(int index);
     public void add(Object toBeAdded);
     public void replace(int index, Object toBeReplaced);
+    public void replaceAll(Object toBeReplaced);
 }
